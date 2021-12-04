6a89404aee40e55f60daa7513f1d0e7ae54f069c
Quba1 <jak.lewand@gmail.com>
2021-12-04 17:33:14 +0100

    added some wip super test

--- src/model/super_tests.rs
+++ src/model/super_tests.rs
@@ -8,9 +8,24 @@
 
 use std::path::Path;
 
-use crate::model::configuration::Config;
+use super::configuration::Config;
+
+use super::environment::{EnvFields, Environment};
 
 #[test]
 fn pressure_interpolation() {
-    let cfg = Config::new_from_file(Path::new("./test-data/config.yaml"));
+    let mut cfg = Config::new_from_file(Path::new("./test-data/config.yaml")).unwrap();
+    let env = Environment::new(&mut cfg).unwrap();
+
+    let (x, y) = env
+        .projection
+        .project(cfg.domain.ref_lon, cfg.domain.ref_lat);
+
+    for z in (250..=10_000).step_by(1) {
+        let v = env
+            .get_field_value(x, y, z as f64, EnvFields::Pressure)
+            .unwrap();
+
+        println!("{:>5.1} {:>5.2}",z as f64, v);
+    }
 }
